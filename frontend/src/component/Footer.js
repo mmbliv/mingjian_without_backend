4d141da6900d8c3a61ccf0ff1f2279f611dc9e06
mjwen <wenxx151@gmail.com>
2022-05-01 23:51:12 -0700

    Update footer info

--- frontend/src/component/Footer.js
+++ frontend/src/component/Footer.js
@@ -3,8 +3,8 @@ import React from "react"
 export const Footer = () => {
   return (
     <div className="h-32 w-full bg-slate-400 flex flex-col justify-center items-center">
-      <p>Location:</p>
-      <p>Contact:acondye@gmail.com</p>
+      <p>Contact</p>
+      <p>Email: mjwen@lbl.gov</p>
     </div>
     // className="bottom-0 relative mt-11 h-32 w-full bg-slate-400 flex flex-col justify-center items-center"
   )
