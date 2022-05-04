1c62e476165389605a7f9fa585d9104a0e8e6816
Min <mmbliv@gmail.com>
2022-05-03 22:58:59 -0700

    footer layout

--- frontend/src/component/Footer.js
+++ frontend/src/component/Footer.js
@@ -3,22 +3,28 @@ import { FaGithubSquare } from "react-icons/fa"
 
 export const Footer = () => {
   return (
-    <div className="h-32 w-full bg-gray-200 footer">
-      <div className="text-center sm:text-left flex flex-col sm:flex-row justify-between items-center max-w-5xl mx-auto h-full">
+    <div className="h-32 w-ful footer bg-gray-200">
+      <div className="flex w-full justify-between items-center max-w-5xl mx-auto h-full ">
         <div>
           <p>Contact</p>
           <p>address</p>
           <p>Email: mjwen@lbl.gov</p>
         </div>
         <div>
-          <p>Website Designed by:</p>
-          <a
-            href="https://github.com/mmbliv"
-            className="flex items-center hover:underline hover:text-Button w-full justify-center sm:justify-start"
-          >
-            <FaGithubSquare />
-            <p className=" pl-2">mmbliv</p>
-          </a>
+          <p>
+            &copy;&nbsp;{new Date().getFullYear()}
+            <span>&nbsp;Mingjian Wen </span>
+          </p>
+          <div className="flex flex-row gap-2">
+            <p>Designed by</p>
+            <a
+              href="https://github.com/mmbliv"
+              className="flex items-center hover:underline hover:text-Button"
+            >
+              <FaGithubSquare />
+              <p className=" pl-2">mmbliv</p>
+            </a>
+          </div>
         </div>
       </div>
     </div>
