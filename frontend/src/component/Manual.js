2e4cea40f06c4d8ddb4fde650d602fdcc4876c16
min <mmbliv@gmail.com>
2022-10-17 21:17:21 -0500

    group manual centered

--- frontend/src/component/Manual.js
+++ frontend/src/component/Manual.js
@@ -3,13 +3,13 @@ import React from "react"
 import manual from "../icon/manual.png"
 export const Manual = () => {
   return (
-    <div className="w-full absolute -bottom-28">
+    <div className="w-full absolute -bottom-28 left-0">
       <a
         href="https://www.chee.uh.edu/faculty/wen-mingjian"
         target="_blank"
         rel="noreferrer noopener"
       >
-        <img src={manual} alt="book" className="w-1/2 m-auto" />
+        <img src={manual} alt="book" className="m-auto w-2/3" />
       </a>
     </div>
   )
