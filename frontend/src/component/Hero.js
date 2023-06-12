48932fa0c4548a43a9bacbcd501ddd3bfa235f16
min <mmbliv@gmail.com>
2023-06-12 14:16:26 -0500

    hero done

--- frontend/src/component/Hero.js
+++ frontend/src/component/Hero.js
@@ -4,17 +4,14 @@ import { GatsbyImage } from "gatsby-plugin-image"
 
 const query = graphql`
   {
-    allStrapiHero {
+    allFile(filter: { relativeDirectory: { eq: "photo/hero_img" } }) {
       nodes {
-        background {
-          localFile {
-            childImageSharp {
-              gatsbyImageData
-            }
-          }
+        relativePath
+        extension
+        publicURL
+        childImageSharp {
+          gatsbyImageData
         }
-        title
-        subtitle
       }
     }
   }
@@ -23,21 +20,23 @@ const query = graphql`
 export const Header = () => {
   const data = useStaticQuery(query)
   const {
-    allStrapiHero: { nodes: hero },
+    allFile: { nodes: hero },
   } = data
   return (
     <div className="mt-11 w-full ">
       <GatsbyImage
-        image={hero[0].background.localFile.childImageSharp.gatsbyImageData}
-        alt={hero[0].title}
+        image={hero[0].childImageSharp.gatsbyImageData}
+        alt="wengroup"
         className="object-cover w-full h-60 opacity-50 block mx-auto"
       />
 
       <div className="relative -top-40 text-center text-4xl tracking-wider w-fit font-bold text-gray-900 max-w-6xl my-0 mx-auto p-1 border-b-2 border-black">
-        <p className="uppercase hero-title">{hero[0].title}</p>
+        <p className="uppercase hero-title">THE WEN GROUP @ U HOUSTON</p>
       </div>
       <div className="relative -top-36 my-0 mx-auto w-full text-center text-gray-900 ">
-        <p className="hero-subtitle">{hero[0].subtitle}</p>
+        <p className="hero-subtitle">
+          Materials Design with AI, Simulation, and Data
+        </p>
       </div>
     </div>
   )
