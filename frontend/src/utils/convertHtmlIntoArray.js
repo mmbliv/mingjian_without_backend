3d88ba0c35e8256a267ff28e28ce5c70f04400a9
min <mmbliv@gmail.com>
2023-06-12 13:17:10 -0500

    added converHtmlIntoArray function

--- frontend/src/utils/convertHtmlIntoArray.js
+++ frontend/src/utils/convertHtmlIntoArray.js
@@ -1,27 +1,40 @@
 import TurndownService from "turndown"
 
-function convertHtmlToArray(html) {
+export function convertHtmlToArray(html) {
   const turndownService = new TurndownService()
-  const parser = new DOMParser()
-  const doc = parser.parseFromString(html, "text/html")
 
-  const dates = doc.querySelectorAll("h1")
-  const contents = doc.querySelectorAll("p")
+  const container = document.createElement("div")
+  container.innerHTML = html
+
+  const entries = container.querySelectorAll("h1, p")
 
   const result = []
+  let currentEntry = null
 
-  for (let i = 0; i < dates.length; i++) {
-    const date = dates[i].textContent
-    const contentHtml = contents[i].innerHTML
-    const contentMarkdown = turndownService.turndown(contentHtml)
-    result.push({ date, content: contentMarkdown })
+  for (let i = 0; i < entries.length; i++) {
+    const entry = entries[i]
+
+    if (entry.tagName === "H1") {
+      if (currentEntry) {
+        result.push(currentEntry)
+      }
+      currentEntry = { date: entry.textContent.trim() }
+    } else if (entry.tagName === "P") {
+      const contentHtml = entry.innerHTML
+      const contentMarkdown = turndownService.turndown(contentHtml)
+      currentEntry.content = contentMarkdown
+    }
+  }
+
+  if (currentEntry) {
+    result.push(currentEntry)
   }
 
   return result
 }
 
-const html =
-  '<h1>2022-05-02</h1>\n<p>Our new Perspective discussing "Chemical Reaction Networks and Opportunities for Machine Learning" appeared in <em>Nature Computational Science</em>!</p>\n<h1>2022-05-06</h1>\n<p>PhD students Wei-Fan, Emmanuel, and Dale join the group. Welcome!</p>'
+// const html =
+//   '<h1>2022-05-02</h1>\n<p>Our new Perspective discussing "Chemical Reaction Networks and Opportunities for Machine Learning" appeared in <em>Nature Computational Science</em>!</p>\n<h1>2022-05-06</h1>\n<p>PhD students Wei-Fan, Emmanuel, and Dale join the group. Welcome!</p>'
 
-const arrayResult = convertHtmlToArray(html)
-console.log(arrayResult)
+// const arrayResult = convertHtmlToArray(html)
+// console.log(arrayResult)
